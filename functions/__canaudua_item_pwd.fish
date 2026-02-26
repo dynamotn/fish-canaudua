a5d7dda78633114902905cb05013ebfc11e66333
Trần Đức Nam <me@dynamotn.dev>
2026-02-26 15:23:37 +0700

    refactor: shorten directory paths in prompt

--- functions/__canaudua_item_pwd.fish
+++ functions/__canaudua_item_pwd.fish
@@ -1,3 +1,15 @@
 function __canaudua_item_pwd
-    printf '%s' (string replace -- $HOME '~' $PWD)
+    set -l home_replaced (string replace -- $HOME '~' $PWD)
+    set -l parts (string split / -- $home_replaced)
+
+    # Truncate intermediate dirs (not first or last) to first 2 characters
+    if test (count $parts) -gt 3
+        set -l truncated
+        for i in (seq 2 (math (count $parts) - 1))
+            set -a truncated (string sub -l 2 -- $parts[$i])
+        end
+        set parts[2..-2] $truncated
+    end
+
+    string join / -- $parts
 end
